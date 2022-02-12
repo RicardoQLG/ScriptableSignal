0253898481bad8d88ba12dcdb0dc3887f429678a
Ricardo Godoy <rq.godoy@gmail.com>
2022-02-12 04:35:55 -0300

    refactor: change test class name

--- Tests/EditorTests/signal_value.cs
+++ Tests/EditorTests/signal_value.cs
@@ -4,17 +4,17 @@ using UnityEngine;
 
 class SutImplementation : SignalValue<float> {}
 
-public class signal_variable
+public class signal_value
 {
     [Test]
-    public void signal_variable_should_instantiate_SignalEvent_if_not_provided()
+    public void signal_value_should_instantiate_SignalEvent_if_not_provided()
     {
         SutImplementation sut = ScriptableObject.CreateInstance<SutImplementation>();
         Assert.IsInstanceOf(typeof(ISignalEvent<float>), sut.OnUpdateValue);
     }
 
     [Test]
-    public void signal_variable_should_be_able_to_set_OnUpdateValue()
+    public void signal_value_should_be_able_to_set_OnUpdateValue()
     {
         SutImplementation sut = ScriptableObject.CreateInstance<SutImplementation>();
         ISignalEvent<float> signalEvent = Substitute.For<ISignalEvent<float>>();
