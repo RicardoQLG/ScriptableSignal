0429bfd583f37f1e53324d278c2d58aa7fe57f36
Ricardo Godoy <rq.godoy@gmail.com>
2022-02-13 05:21:53 -0300

    refactor: add value namespace

--- Tests/EditorTests/signal_value.cs
+++ Tests/EditorTests/signal_value.cs
@@ -2,45 +2,47 @@ using NSubstitute;
 using NUnit.Framework;
 using UnityEngine;
 
-class SutImplementation : SignalValue<float> {}
+namespace Value {
+    class SutImplementation : SignalValue<float> {}
 
-public class signal_value
-{
-    private SutImplementation sut;
-    private ISignalEvent<float> signalEventStub;
-
-    [SetUp]
-    public void SetUp ()
+    public class signal_value
     {
-        sut = ScriptableObject.CreateInstance<SutImplementation>();
-        signalEventStub = Substitute.For<ISignalEvent<float>>();
-    }
+        private SutImplementation sut;
+        private ISignalEvent<float> signalEventStub;
 
-    [Test]
-    public void signal_value_should_instantiate_SignalEvent_if_not_provided()
-    {
-        Assert.IsInstanceOf(typeof(ISignalEvent<float>), sut.OnUpdateValue);
-    }
+        [SetUp]
+        public void SetUp ()
+        {
+            sut = ScriptableObject.CreateInstance<SutImplementation>();
+            signalEventStub = Substitute.For<ISignalEvent<float>>();
+        }
 
-    [Test]
-    public void signal_value_should_be_able_to_set_OnUpdateValue()
-    {
-        sut.OnUpdateValue = signalEventStub;
-        Assert.AreSame(signalEventStub, sut.OnUpdateValue);
-    }
+        [Test]
+        public void signal_value_should_instantiate_SignalEvent_if_not_provided()
+        {
+            Assert.IsInstanceOf(typeof(ISignalEvent<float>), sut.OnUpdateValue);
+        }
 
-    [Test]
-    public void signal_value_should_be_able_to_set_Value()
-    {
-        sut.Value = 7f;
-        Assert.AreEqual(7f, sut.Value);
-    }
+        [Test]
+        public void signal_value_should_be_able_to_set_OnUpdateValue()
+        {
+            sut.OnUpdateValue = signalEventStub;
+            Assert.AreSame(signalEventStub, sut.OnUpdateValue);
+        }
 
-    [Test]
-    public void signal_value_should_call_Invoke_with_value()
-    {
-        sut.OnUpdateValue = signalEventStub;
-        sut.Value = 15f;
-        signalEventStub.Received().Invoke(15f);
+        [Test]
+        public void signal_value_should_be_able_to_set_Value()
+        {
+            sut.Value = 7f;
+            Assert.AreEqual(7f, sut.Value);
+        }
+
+        [Test]
+        public void signal_value_should_call_Invoke_with_value()
+        {
+            sut.OnUpdateValue = signalEventStub;
+            sut.Value = 15f;
+            signalEventStub.Received().Invoke(15f);
+        }
     }
 }
