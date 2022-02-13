6e970d563ce4d5334b5c2a1fb2ccb37449b2d1b5
Ricardo Godoy <rq.godoy@gmail.com>
2022-02-13 04:06:05 -0300

    feat: add signal value editor

---
+++ Editor/SignalValueEditor.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEditor;
+
+public class SignalValueEditor<T> : Editor {
+    private SerializedProperty m_ValueProp;
+    private SerializedProperty m_OnUpdateValueProp;
+
+    private void OnEnable()
+    {
+        m_ValueProp = serializedObject.FindProperty("m_Value");
+        m_OnUpdateValueProp = serializedObject.FindProperty("m_OnUpdateValue");
+    }
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        serializedObject.Update();
+
+        EditorGUILayout.PropertyField(m_ValueProp, new GUIContent("Value"));
+
+        
+        if (serializedObject.hasModifiedProperties)
+        {
+            serializedObject.ApplyModifiedProperties();
+            SignalValue<T> signal = target as SignalValue<T>;
+            signal.OnUpdateValue.Invoke(signal.Value);
+        }
+
+    }
+}
