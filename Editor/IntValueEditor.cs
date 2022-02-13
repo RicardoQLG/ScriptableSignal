4f004e818f0daa7fc9fffd7662940654939418a9
Ricardo Godoy <rq.godoy@gmail.com>
2022-02-13 05:34:22 -0300

    feat: add int implementations

---
+++ Editor/IntValueEditor.cs
@@ -0,0 +1,4 @@
+using UnityEditor;
+
+[CustomEditor(typeof(IntValue))]
+public class IntValueEditor : SignalValueEditor<int> {}
---
+++ Runtime/IntValue.cs
@@ -0,0 +1,4 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Int Value", menuName = "ScriptableSignal/Int Value", order = 0)]
+public class IntValue : SignalValue<int> {}
